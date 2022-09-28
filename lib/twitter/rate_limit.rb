526065ff53ff43207742794bda160723603ba613
Rohan Mitchell <rohan@rohanmitchell.com>
2022-09-28 10:42:15 +1000

    Make rate limit handling case-insensitive to headers

--- lib/twitter/rate_limit.rb
+++ lib/twitter/rate_limit.rb
@@ -6,21 +6,21 @@ module Twitter
 
     # @return [Integer]
     def limit
-      limit = @attrs['x-rate-limit-limit']
+      limit = attrs['x-rate-limit-limit']
       limit&.to_i
     end
     memoize :limit
 
     # @return [Integer]
     def remaining
-      remaining = @attrs['x-rate-limit-remaining']
+      remaining = attrs['x-rate-limit-remaining']
       remaining&.to_i
     end
     memoize :remaining
 
     # @return [Time]
     def reset_at
-      reset = @attrs['x-rate-limit-reset']
+      reset = attrs['x-rate-limit-reset']
       Time.at(reset.to_i).utc if reset
     end
     memoize :reset_at
@@ -30,5 +30,11 @@ module Twitter
       [(reset_at - Time.now).ceil, 0].max if reset_at
     end
     alias retry_after reset_in
+
+  private
+
+    def attrs
+      @attrs.collect { |k, v| [k.downcase, v] }.to_h
+    end
   end
 end
